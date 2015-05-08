106977b718cd0cbe5ba669d675a24b7fe28cda30
jaynus <jaynus@gmail.com>
2015-05-07 17:00:44 -0700

    initial commit.

---
+++ addons/mfd/script_component.hpp
@@ -0,0 +1,12 @@
+#define COMPONENT mfd
+#include "\z\ace\addons\main\script_mod.hpp"
+
+#ifdef DEBUG_ENABLED_MFD
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_ENABLED_MFD
+    #define DEBUG_SETTINGS DEBUG_ENABLED_MFD
+#endif
+
+#include "\z\ace\addons\main\script_macros.hpp"
