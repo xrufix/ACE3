30f1df7f4c77ae752665cd5fafaaf1f3b5fb8bf7
jokoho48 <hoffmann.jonas@gmail.com>
2015-08-02 22:02:34 +0200

    Add ACE Crate

---
+++ addons/crate/functions/script_component.hpp
@@ -0,0 +1,1 @@
+#include "\z\ace\addons\crate\script_component.hpp"
---
+++ addons/crate/script_component.hpp
@@ -0,0 +1,12 @@
+#define COMPONENT crate
+#include "\z\ace\addons\main\script_mod.hpp"
+
+#ifdef DEBUG_ENABLED_CRATE
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_CRATE
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_BLANK
+#endif
+
+#include "\z\ace\addons\main\script_macros.hpp"
